9c26c4b1f05489574e549b4e39a4ca6c9a2adae6
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-17 10:45:51 +0900

    feat: usersテーブルへseedingを行えるように作成(#9)

---
+++ prisma/seed/users.js
@@ -0,0 +1,27 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function usersSeeding() {
+  await prisma.users.deleteMany({});
+
+  await prisma.users.createMany({
+    data: [
+      {
+        team_id: 5,
+        name: "山田　太郎",
+      },
+      {
+        team_id: 1,
+        name: "佐藤　花子",
+      },
+      {
+        team_id: 4,
+        name: "鈴木　次郎",
+      },
+    ],
+  });
+
+  console.log("insert users data successfully!!");
+}
+
+module.exports = usersSeeding;
