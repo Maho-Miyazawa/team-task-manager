5ba41f609e5bec01a60c2bfa95744494e81106cf
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-17 11:18:51 +0900

    feat: notesテーブルにseedingできるようにプソグラムを追加(#10)

--- prisma/seed/index.js
+++ prisma/seed/index.js
@@ -1,8 +1,18 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
 const usersSeeding = require("./users");
+const notesSeeding = require("./notes");
 
 usersSeeding()
+  .catch((err) => {
+    throw err;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+notesSeeding()
   .catch((err) => {
     throw err;
   })
