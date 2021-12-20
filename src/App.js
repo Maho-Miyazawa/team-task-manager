ee614cb5cb43c4e7cd0475d5993ba172362fef92
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-20 09:29:17 +0900

    feat: フロントにデータベースのデータをテストで表示(#15)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+import { useState } from "react";
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    Users {
+      id
+      team_id
+      name
+    }
+  }
+`;
 
 function App() {
+  const [searchNameInput, setSearchNameInput] = useState("");
+  const [searchNameConfirm, setSearchNameConfirm] = useState("");
+
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  function searchNameChange(e) {
+    setSearchNameInput(e.target.value);
+  }
+  function searchNameClick() {
+    setSearchNameConfirm(searchNameInput);
+    setSearchNameInput("");
+  }
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {data.Users.map((user) => {
+          return (
+            <div key={user.id}>
+              ID: {user.id}, Team ID:{user.team_id}, Name: {user.name}
+            </div>
+          );
+        })}
+      </div>
+      <input type="text" onChange={searchNameChange} value={searchNameInput} />
+      <button type="button" onClick={searchNameClick}>
+        検索
+      </button>
+      <div>{searchNameConfirm}</div>
     </div>
   );
 }
