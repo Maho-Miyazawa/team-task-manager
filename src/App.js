afa73fcd5eebbfdccdf0ac0fc3baa18003b67965
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-20 14:04:27 +0900

    feat: データベースのデータから1名選択できるように機能を追加(#15, #22)

--- src/App.js
+++ src/App.js
@@ -1,51 +1,94 @@
 import "./App.css";
 import { useQuery, gql } from "@apollo/client";
-import { useState } from "react";
-
-const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    Users {
-      id
-      team_id
-      name
-    }
-  }
-`;
+import { useState, useRef } from "react";
 
 function App() {
   const [searchNameInput, setSearchNameInput] = useState("");
   const [searchNameConfirm, setSearchNameConfirm] = useState("");
+  const [user, setUser] = useState();
+  const inputEl = useRef(null);
 
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  const GET_NAME = gql`
+    query  {
+      Users(name: "${searchNameConfirm}") {
+        id
+        team_id
+        name
+      }
+    }
+`;
 
-  function searchNameChange(e) {
+  const { loading, error, data } = useQuery(GET_NAME);
+
+  function searchStart() {
+    setSearchNameConfirm(searchNameInput);
+    inputEl.current.focus();
+  }
+
+  function nameInputChange(e) {
     setSearchNameInput(e.target.value);
   }
-  function searchNameClick() {
-    setSearchNameConfirm(searchNameInput);
-    setSearchNameInput("");
+
+  function searchButtonClick(e) {
+    e.preventDefault();
+    searchStart();
+  }
+  function enterClick(e) {
+    if (e.key === "Enter") {
+      searchStart();
+    }
+  }
+
+  function userChooseButtonClick(e) {
+    setUser(e.target.value);
   }
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (error) return <p>Error</p>;
 
   return (
-    <div>
-      <div>
-        {data.Users.map((user) => {
-          return (
-            <div key={user.id}>
-              ID: {user.id}, Team ID:{user.team_id}, Name: {user.name}
-            </div>
-          );
-        })}
-      </div>
-      <input type="text" onChange={searchNameChange} value={searchNameInput} />
-      <button type="button" onClick={searchNameClick}>
-        検索
-      </button>
-      <div>{searchNameConfirm}</div>
-    </div>
+    <>
+      {user && <p>Good{user}</p>}
+      {!user && (
+        <div>
+          <input
+            type="text"
+            onChange={nameInputChange}
+            onKeyDown={enterClick}
+            value={searchNameInput}
+            ref={inputEl}
+          />
+          <button type="button" onClick={searchButtonClick}>
+            検索
+          </button>
+          <div>
+            {data.Users.length === 0 ? (
+              <p>検索結果はありませんでした</p>
+            ) : (
+              <table>
+                <tbody>
+                  {data.Users.map((user) => {
+                    return (
+                      <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>
+                          <button
+                            value={user.id}
+                            onClick={userChooseButtonClick}
+                          >
+                            Click!!
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
