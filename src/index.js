a15c3d570c12d0212b9d65753186ba4b15ee47d8
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-21 17:14:28 +0900

    fix: @apollo/clientを削除(#32)

--- src/index.js
+++ src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "/graphql",
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
