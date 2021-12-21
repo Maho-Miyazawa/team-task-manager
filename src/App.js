50bd420a36759e130d78db3c7f00cbe5e7828d00
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-21 14:09:59 +0900

    feat: フロント画面にUser(notesデータ含む)を表示できるようにする(#29)

--- src/App.js
+++ src/App.js
@@ -3,130 +3,60 @@ import { useQuery, gql } from "@apollo/client";
 import { useState, useRef } from "react";
 
 function App() {
-  const [searchNameInput, setSearchNameInput] = useState("");
-  const [searchNameConfirm, setSearchNameConfirm] = useState("");
-  const [userId, setUserId] = useState(0);
-  const inputEl = useRef(null);
-
   const GET_DATA = gql`
-    query  {
-      Users(name: "${searchNameConfirm}") {
+    query {
+      User(id: 1) {
         id
         team_id
         name
-      }
-      Notes(user_id: ${userId}) {
-        id
-        user_id
-        note
-        progress
-        priority
-        is_deleted
+        notes {
+          id
+          note
+          progress
+          priority
+          is_deleted
+        }
       }
     }
-`;
+  `;
 
   const { loading, error, data } = useQuery(GET_DATA);
-
-  function searchStart() {
-    setSearchNameConfirm(searchNameInput);
-    inputEl.current.focus();
-  }
-
-  function nameInputChange(e) {
-    setSearchNameInput(e.target.value);
-  }
-
-  function searchButtonClick(e) {
-    e.preventDefault();
-    searchStart();
-  }
-  function enterClick(e) {
-    if (e.key === "Enter") {
-      searchStart();
-    }
-  }
-
-  function userChooseButtonClick(e) {
-    console.log(e.target.value);
-    setUserId(e.target.value);
-  }
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
+  function handleClick() {
+    console.log(data.User);
+  }
+
   return (
     <>
-      {userId === 0 && (
-        <div>
-          <input
-            type="text"
-            onChange={nameInputChange}
-            onKeyDown={enterClick}
-            value={searchNameInput}
-            ref={inputEl}
-          />
-          <button type="button" onClick={searchButtonClick}>
-            検索
-          </button>
-          <div>
-            {data.Users.length === 0 ? (
-              <p>検索結果はありませんでした</p>
-            ) : (
-              <table>
-                <tbody>
-                  {data.Users.map((user) => {
-                    return (
-                      <tr key={user.id}>
-                        <td>{user.name}</td>
-                        <td>
-                          <button
-                            value={user.id}
-                            onClick={userChooseButtonClick}
-                          >
-                            Click!!
-                          </button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            )}
-          </div>
-        </div>
-      )}
-      {userId !== 0 && data.Notes.length === 0 && (
-        <p>Notesはありませんでした</p>
-      )}
-      {userId !== 0 && data.Notes.length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              <td>ID</td>
-              <td>User_id</td>
-              <td>Notes</td>
-              <td>進捗度</td>
-              <td>優先度</td>
-              <td>削除フラグ</td>
-            </tr>
-          </thead>
-          <tbody>
-            {data.Notes.map((note) => {
-              return (
-                <tr key={note.id}>
-                  <td>{note.id}</td>
-                  <td>{note.user_id}</td>
-                  <td>{note.note}</td>
-                  <td>{note.progress}</td>
-                  <td>{note.priority}</td>
-                  <td>{note.is_deleted}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      )}
+      <div>Good</div>
+      <button onClick={handleClick}>Click Me!!!</button>
+      <table>
+        <tbody>
+          <tr>
+            <th>ID</th>
+            <>{data.User ? <td>{data.User.id}</td> : <td></td>}</>
+          </tr>
+          <tr>
+            <th>Name</th>
+            <>{data.User ? <td>{data.User.name}</td> : <td></td>}</>
+          </tr>
+          <tr>
+            <th>Notes</th>
+            <>
+              {data.User && data.User.notes.length > 0 ? (
+                data.User.notes.map((note) => {
+                  return <td key={note.id}>{note.note}</td>;
+                })
+              ) : (
+                <td></td>
+              )}
+            </>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 }
