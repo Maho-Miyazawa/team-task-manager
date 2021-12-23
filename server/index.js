1c792c13c78dfeb5838d9b7f0ef0257a3ad34b92
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-23 13:06:43 +0900

    fix: apollo-serverをapollo-server-express(#68)

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
 const express = require("express");
 const http = require("http");
+const path = require("path");
 
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
@@ -9,6 +10,7 @@ const resolvers = require("./resolvers");
 async function startApolloServer() {
   const app = express();
   const httpServer = http.createServer(app);
+  app.use(express.static(path.resolve(__dirname, "..", "build")));
 
   const server = new ApolloServer({
     typeDefs,
