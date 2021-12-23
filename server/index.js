a22c4e76fbada8a4c17cc76ad43666bb32aad431
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-23 11:47:08 +0900

    chore: graphqlのLPを無効化

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const { ApolloServer } = require("apollo-server");
+const { ApolloServerPluginLandingPageDisabled } = require("apollo-server-core");
+
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginLandingPageDisabled()],
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
