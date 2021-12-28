7ab4f4c30fb87743ea3ad2b1baf511e056c4863d
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-28 11:37:15 +0900

    feat: Auth0を使用しログアウトボタンを実装(#84)

---
+++ src/components/LogoutButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+  const logoutButtonClick = () => logout({ returnTo: window.location.origin });
+
+  return (
+    <div>
+      <button onClick={logoutButtonClick}>ログアウト</button>
+    </div>
+  );
+};
+
+export default LogoutButton;
