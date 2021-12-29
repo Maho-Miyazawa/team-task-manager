54a3a2e6b3ddcb1bc3e8ebeaf3b0a31807b8c963
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-29 15:40:30 +0900

    feat: サインアップ時にユーザー名とチーム名を入力するRedux周りを作成(#53, #92)

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../slices/userSlice";
 import taskReducer from "../slices/taskSlice";
+import signupReducer from "../slices/signupSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
     task: taskReducer,
+    signup: signupReducer,
   },
 });
