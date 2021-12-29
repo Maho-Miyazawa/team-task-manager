1c3a1e071cdb517bc71c735cc50d01f89467f086
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-29 20:43:19 +0900

    feat: トップページをHomeコンポーネントに設定(#96)

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import LoginButton from "./LoginButton";
+
+function Home() {
+  return (
+    <div>
+      <h1>Team Task Manager</h1>
+      <LoginButton />
+    </div>
+  );
+}
+
+export default Home;
