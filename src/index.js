05345945b8660182faf417c0a5c33cbf51baa159
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-29 20:34:34 +0900

    fix: ログイン後のリダイレクトURLを/から/my-pageに変更(#96)

--- src/index.js
+++ src/index.js
@@ -4,20 +4,25 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 
 import store from "./store/store";
 import { Provider } from "react-redux";
+
+const redirectUri = `${window.location.origin}/my-page`;
 
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH_DOMAIN}
       clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
-      redirectUri={window.location.origin}
+      redirectUri={redirectUri}
     >
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
     </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
