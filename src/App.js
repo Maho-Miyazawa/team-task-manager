ee2ebc5c0b00a20d7ac1d404388edddfb06cefab
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-31 01:11:03 +0900

    refactor: App.jsとcssをリファクタリング(#96)

--- src/App.js
+++ src/App.js
@@ -1,50 +1,20 @@
 import "./App.css";
-import { useSelector, useDispatch } from "react-redux";
-import { changeUserId, fetchUserData } from "./slices/userSlice";
-import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
+import { Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
-import User from "./components/User";
-import CreateTask from "./components/CreateTask";
-import Tasks from "./components/Tasks";
+import MyPage from "./components/MyPage";
+import Home from "./components/Home";
+import Member from "./components/Member";
 
 function App() {
-  const dispatch = useDispatch();
-  const userId = useSelector((state) => state.user.userId);
-  const userData = useSelector((state) => state.user.userData);
-
-  // TODO 検索入力欄がnullの時のエラー解消
-  const getUser = () => dispatch(fetchUserData(userId || userData.id));
-  const handleChangeUserId = (e) =>
-    dispatch(changeUserId(String(e.target.value)));
-
-  function handleSearchButtonClick(e) {
-    e.preventDefault();
-    getUser();
-  }
-
   return (
     <div className="wrapper">
-      <LoginButton />
-      <LogoutButton />
-      <Profile />
-      <Signup />
-      <input type="text" onChange={handleChangeUserId} />
-      <button onClick={handleSearchButtonClick}>検索</button>
-      <CreateTask />
-      <>
-        {userData ? (
-          <div className="main">
-            <User />
-            <div className="tasks-container">
-              <Tasks />
-            </div>
-          </div>
-        ) : (
-          <div>存在しません</div>
-        )}
-      </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="my-page" element={<MyPage />} />
+        <Route path="member" element={<Member />} />
+        {/* <Route path="member/tasks" element={<MemberTasks />} /> */}
+      </Routes>
     </div>
   );
 }
