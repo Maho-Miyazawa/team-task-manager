635bdac92c9b40703f7920d59ce6c7c2b1f1ae15
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2021-12-30 21:34:44 +0900

    fix: ページ更新時にユーザープロフィールが表示されないバグ改修(#101)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,45 @@
 import LogoutButton from "./LogoutButton";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
+import { setProfileUserName, setProfileTeamName } from "../slices/signupSlice";
+import axios from "axios";
 
 function Header() {
+  const dispatch = useDispatch();
   const profileUserName = useSelector((state) => state.signup.profileUserName);
   const profileTeamName = useSelector((state) => state.signup.profileTeamName);
+  const { user } = useAuth0();
+
+  const collateUserId = async () => {
+    try {
+      const collation = await axios({
+        method: "POST",
+        url: "/graphql",
+        data: {
+          query: `query {
+                    CollateUserId(id: "${user.sub}") {
+                        name
+                        team {
+                            name
+                        }
+                    }
+                }`,
+        },
+      });
+
+      let collationData = collation.data.data.CollateUserId;
+
+      dispatch(setProfileUserName(collationData.name));
+      dispatch(setProfileTeamName(collationData.team.name));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    collateUserId();
+  });
 
   return (
     <div className="header">
