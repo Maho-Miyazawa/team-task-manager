15e6fe2b46079b6b5ccb26864d67a892124a7214
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2022-01-04 19:14:29 +0900

    fix: サインアップ機能を削除(#122)

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,6 @@ import LoginButton from "./LoginButton";
 import topPageImg from "../images/img-top-page.png";
 
 function Home() {
-  const signup = (e) => {
-    e.preventDefault();
-    const data = `https://${process.env.REACT_APP_AUTH_DOMAIN}/authorize?scope=openid profile email&response_type=code&response_mode=query&client_id=${process.env.REACT_APP_AUTH_CLIENT_ID}&redirect_uri=${window.location.origin}/signup&prompt=login&screen_hint=signup`;
-    window.location.href = data;
-  };
-
   return (
     <div className="top-page-wrapper">
       <div className="top-title-container">
@@ -23,11 +17,8 @@ function Home() {
             <br />
             さあ、チームコミュニケーションを活発化しましょう！
           </p>
-          <div className="top-page-login-signup-area">
+          <div>
             <LoginButton />
-            <button className="signup-button" onClick={signup}>
-              新規登録
-            </button>
           </div>
         </div>
       </div>
